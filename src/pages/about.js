3dd8d0280f9670e2c12d004c8ad2f6b1fac07cfc
Josh Cox <joshuacox@users.noreply.github.com>
2018-04-28 10:38:01 -0500

    fixed #11 (#12)
    S&M

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ export default () => (
   <div>
     <h1>About Legitblock </h1>
     <p>
-  Legitblock aims to integrate git and legal documents.
+  Legitblock aims to integrate the blockchain, git, organization voting and legal documents.
   Git blame just took on a whole new level of meaning!
     </p>
   </div>
